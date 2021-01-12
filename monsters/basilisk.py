0958906e9fa798af13e10e14465f84b476a6b149
Collin Fairlie <collin.fairlie@gmail.com>
2021-01-12 12:42:41 -0800

    Added notes for later work

--- monsters/basilisk.py
+++ monsters/basilisk.py
@@ -7,6 +7,7 @@ def basilisk_defeated():
 
 def basilisk_encountered(inventory):
     print("""
+You quickly reach for the door behind you, but the door has vanished, leaving no exit.
 The gigantic basilisk, slithers across the room and opens its mouth with a hiss.
 You slide under a desk, barely avoiding the deadly bite of the serpent.
 You must act quickly if you wish to survive!
@@ -14,3 +15,6 @@ You must act quickly if you wish to survive!
 
     return defeated
 
+
+# **********************************
+# Add a way for player to open the door again to exit at the end
