c1f52756ec6b47e39925736d7932176d97bfa58a
Collin Fairlie <collin.fairlie@gmail.com>
2021-01-13 16:37:57 -0800

    Continued building of scenarios. Currently retunrs easily, but story can be build more.

--- monsters/dementors.py
+++ monsters/dementors.py
@@ -5,10 +5,45 @@ def dementors_defeated():
     global defeated
     defeated = True
 
-def scenario1():
-    print("testing")
+def scenario1(player):
+    print("""
+You pull out your wand and hold it at the ready.
+What spell will you cast to dispel of the dementors?
+Enter "A" for Locomotor Wibbly to cause the dementors legs to collapse beneath them.
+"B" for Nebulus to create a fog from the tip of your wand.
+"C" for Expecto Patronum to cast a Patronus Charm filled with positive emotions.
+"D" for Diffindo to send a cutting charm at the dementors.
+    """)
 
-def dementors_encountered(inventory):
+    print('---------------------------')
+    answer = input(">").strip().upper()
+
+    if answer == 'A':
+        print("""
+Your spell shoots through the dementors as if nothing had happened.
+A dementor comes from behind, knocking your wand from your hand and into a far away bush.
+        """)
+        loser()
+    elif answer == 'B':
+        print("""
+A thick fog presses from the tip of your wand, filling the Training Field, so much so that you can no longer see the dementors.
+One dementor claws at your back, cutting deep into your skin and causing you to fall to the ground.
+
+        """)
+        player.health -= 10
+        dementors_defeated()
+
+
+def loser():
+    print("""
+    With no defenses, you trimmer in fear! Dementors sweep in to suck the life away from you.
+
+    GAME OVER
+    """)
+    return
+
+
+def dementors_encountered(inventory, player):
     print("""
 As you enter the Training Grounds, the light around you begins to slip away.
 Happiness drains from you as fear shivers down the length of your spine.
@@ -18,13 +53,9 @@ You must be cleaver against these dark creatures, for only magic will for them t
     """)
 
     if 'wand' in inventory:
-        scenario1()
+        scenario1(player)
     else:
-        print("""
-        With no defenses, you trimmer in fear! Dementors sweep in to suck the life away from you.
-        
-        GAME OVER
-        """)
+        loser()
         return
 
     return defeated
