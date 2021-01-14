a96e59bdaa625c62f2c1fce6a9cb6536f12e8baa
Collin Fairlie <collin.fairlie@gmail.com>
2021-01-13 16:01:46 -0800

    Working out setup for fighting scenarios.

--- monsters/basilisk.py
+++ monsters/basilisk.py
@@ -5,6 +5,11 @@ def basilisk_defeated():
     global defeated
     defeated = True
 
+def scenario1():
+    print("""
+
+    """)
+
 def basilisk_encountered(inventory):
     print("""
 You quickly reach for the door behind you, but the door has vanished, leaving no exit.
@@ -12,6 +17,15 @@ The gigantic basilisk, slithers across the room and opens its mouth with a hiss.
 You slide under a desk, barely avoiding the deadly bite of the serpent.
 You must act quickly if you wish to survive!
     """)
+
+    if 'wand' in inventory:
+        scenario1()
+    else:
+        print("""
+        With no defenses, you trimmer in fear, only to be engulfed by the flames of the dragon.
+        \nGAME OVER
+                """)
+        return
 
     return defeated
 
